e7014c08ccc5c4b4071e5e5c6ed23bc2e6b51644
NoahY <noahy57@gmail.com>
2013-08-07 14:54:20 -0500

    version

--- qa-plugin.php
+++ qa-plugin.php
@@ -5,7 +5,7 @@
         Plugin URI: https://github.com/NoahY/q2a-book
         Plugin Update Check URI: https://raw.github.com/NoahY/q2a-book/master/qa-plugin.php
         Plugin Description: Makes boook from top questions and answers
-        Plugin Version: 0.7
+        Plugin Version: 0.8
         Plugin Date: 2012-03-05
         Plugin Author: NoahY
         Plugin Author URI:                              
